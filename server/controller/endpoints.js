5f331146219520e026a52ffd2bd28e1c76559dda
fauhokama <fauhokama@gmail.com>
2021-04-05 20:29:02 -0300

    Creating some API endpoints

--- server/controller/endpoints.js
+++ server/controller/endpoints.js
@@ -1,11 +1,20 @@
 // Endpoints for React
-const router = require('express').Router();
+const endpoints = require('express').Router();
+const redis = require('../cache/redis')
 
-router.get('/userinfo', (req, res) => {
+endpoints.get('/userinfo', (req, res) => {
+    const getUserInfo = async () => {
+        const user = await redis.getAsync('userinfo');
+        res.send(user);
+    }
+    getUserInfo();
+
 });
 
-router.get('/products', (req, res) => {
-});
+// router.get('/products', (req, res) => {
+// });
 
-router.get('/', (req, res) => {
-});
+// router.get('/', (req, res) => {
+// });
+
+module.exports = endpoints;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const index = require('./controller/index');
+const cors = require('cors');
 
 // Init Express
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Dependencies/Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', index);
